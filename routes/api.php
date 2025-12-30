c2e699fb0b3ff06ca5c3ff46f1bf3fe97e258a60
Tech Mantra <techmantrafirebase@gmail.com>
2025-12-30 11:07:58 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -451,3 +451,6 @@ Route::get('retailer/transaction/remove', [UserController::class, 'transactionRe
 Route::get('stores/wallet/update', [UserController::class, 'storewalletUpdate'])->name('stores.wallet.update');
 
 Route::get('stores/closing/balance/update', [UserController::class, 'storeclosingbalanceUpdate'])->name('stores.closing.balance.update');
+
+
+Route::get('transaction/duplicate/check', [UserController::class, 'duplicateCheck'])->name('duplicate.check');
