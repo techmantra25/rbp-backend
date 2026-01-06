fb7ea220a95d8ff512bdecea5d0da5860571dd9a
Tech Mantra <techmantrafirebase@gmail.com>
2026-01-06 13:45:25 +0530

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -335,7 +335,8 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::get('/', [BarcodeController::class, 'qrRedeem'])->name('index');
             Route::get('/list/csv/export', [BarcodeController::class, 'qrRedeemcsvExport'])->name('csv.export');
             Route::post('/remove', [BarcodeController::class, 'qrRedeemRemove'])->name('remove');
-            
+            Route::post('/edit', [BarcodeController::class, 'qrRedeemEdit'])->name('edit');
+            Route::post('/delete', [BarcodeController::class, 'qrRedeemDelete'])->name('delete');
           
 
         });
